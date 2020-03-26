2241d44dfd9d7824ba20bca903664759ac11f64f
Naina Agrawal <naina.agrawal_cs18@gla.ac.in>
2020-03-26 23:23:58 +0530

    inserted getter setter for fields

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -11,10 +11,34 @@ public class TreeNode {
     private TreeNode left;
     private  TreeNode right;
 
-    private TreeNode(int data)
+    public TreeNode(int data)
     {
         this.data=data;
         left=null;
         right=null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
     }
 }
