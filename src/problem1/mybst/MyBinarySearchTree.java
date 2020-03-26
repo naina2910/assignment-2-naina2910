7ab733aeda9ab7607f0ccba14833a8d7ca7e94f0
Naina Agrawal <naina.agrawal_cs18@gla.ac.in>
2020-03-26 23:58:41 +0530

    insert method completed

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -20,6 +20,35 @@ public class MyBinarySearchTree
 
     public void insert(int data)
     {
-
+        TreeNode node= new TreeNode(data);
+        if(root == null)
+        {
+            root=node;
+        }
+        else
+        {
+            TreeNode temp=root;
+            TreeNode parent=null;
+            while( temp != null)
+            {
+                parent=temp;
+                if( data<= temp.getData())
+                {
+                    temp=temp.getLeft();
+                }
+                else
+                {
+                    temp=temp.getRight();
+                }
+            }
+            if(node.getData()<= parent.getData())
+            {
+                parent.setLeft(node);
+            }
+            else
+            {
+                parent.setRight(node);
+            }
+        }
     }
 }
