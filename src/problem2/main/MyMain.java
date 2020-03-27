395a9b7eda53956a8da3b9e27f6004296b7dadb8
Naina Agrawal <naina.agrawal_cs18@gla.ac.in>
2020-03-27 16:19:17 +0530

    main method created for prob2

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,16 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
+
+import problem1.mybst.MyBinarySearchTree;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
-public class MyMain {
+public class MyMain
+{
+    public static void main(String[] args)
+    {
+
+    }
 }
