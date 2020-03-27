f566449832cf58894b8e368beefac5a5eb8ae43c
Naina Agrawal <naina.agrawal_cs18@gla.ac.in>
2020-03-27 23:46:04 +0530

    tree created for prob4

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,11 +5,22 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain
 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
+        MyBinarySearchTree tree=new MyBinarySearchTree();
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
     }
 }
