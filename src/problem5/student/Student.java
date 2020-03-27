0ad3dc15d330695be1356591986a6d346d51c067
Naina Agrawal <naina.agrawal_cs18@gla.ac.in>
2020-03-27 18:36:50 +0530

    constructor and getter setter made for fields

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -8,5 +8,46 @@ package problem5.student;
 // to store student information and properties
 public class Student
 {
-    
+    private int rollNo;
+    private String name;
+    private int backLogCounter;
+    private int appearingCount;
+
+    public Student(int rollNo, String name)
+    {
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBackLogCounter() {
+        return backLogCounter;
+    }
+
+    public void setBackLogCounter(int backLogCounter) {
+        this.backLogCounter = backLogCounter;
+    }
+
+    public int getAppearingCount() {
+        return appearingCount;
+    }
+
+    public void setAppearingCount(int appearingCount) {
+        this.appearingCount = appearingCount;
+    }
 }
