4b8cf92d651dc60327a9215635c328ea5e8394cc
Naina Agrawal <naina.agrawal_cs18@gla.ac.in>
2020-03-27 16:21:48 +0530

    tree created and inserted values for prob2

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -15,6 +15,13 @@ public class MyMain
 {
     public static void main(String[] args)
     {
-
+        MyBinarySearchTree tree=new MyBinarySearchTree();
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
     }
 }
